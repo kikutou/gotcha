0343dc4123c28e4ee56fc92654b4b61c7187c17f
chenjunyan <kitinc198744@hotmail.com>
2021-03-22 18:44:41 +0900

    link修正

--- resources/views/play/index.blade.php
+++ resources/views/play/index.blade.php
@@ -77,7 +77,7 @@
 
                         <div class="text-center">
                             <div>
-                                <a href="{{ route('get_play_result', ['id' => $gotcha->id]) }}?uid={{ $uid }}?api_token={{ $api_token }}">はい</a>
+                                <a href="{{ route('get_play_result', ['id' => $gotcha->id]) }}?uid={{ $uid }}&api_token={{ $api_token }}">はい</a>
                             </div>
                             <div>
                                 <button type="button" class="btn btn-primary" data-dismiss="modal">いいえ</button>
