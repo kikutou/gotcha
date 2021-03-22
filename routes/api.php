d8a542901658afdb3909d46a71ae80fa54122004
chenjunyan <kitinc198744@hotmail.com>
2021-03-22 18:14:00 +0900

    api対応戻す２

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('/add_user_ticket', 'App\Http\Controllers\Api\GotchaController@add_user_ticket')->name('post_add_user_ticket');
-// Route::post('/add_user_ticket', 'App\Http\Controllers\Api\GotchaController@add_user_ticket')->name('post_add_user_ticket')->middleware("check_user_authentication");
-// Route::post('/disp_gotchas', 'App\Http\Controllers\Api\GotchaController@disp_gotchas')->name('post_disp_gotchas')->middleware("check_user_authentication");
-// Route::post('/gotcha_detail', 'App\Http\Controllers\Api\GotchaController@gotcha_detail')->name('post_gotcha_detail')->middleware("check_user_authentication");
-// Route::post('/gotcha_result', 'App\Http\Controllers\Api\GotchaController@gotcha_result')->name('post_gotcha_result')->middleware("check_user_authentication");
+
+Route::post('/add_user_ticket', 'App\Http\Controllers\Api\GotchaController@add_user_ticket')->name('post_add_user_ticket')->middleware("check_user_authentication");
+Route::post('/disp_gotchas', 'App\Http\Controllers\Api\GotchaController@disp_gotchas')->name('post_disp_gotchas')->middleware("check_user_authentication");
+Route::post('/gotcha_detail', 'App\Http\Controllers\Api\GotchaController@gotcha_detail')->name('post_gotcha_detail')->middleware("check_user_authentication");
+Route::post('/gotcha_result', 'App\Http\Controllers\Api\GotchaController@gotcha_result')->name('post_gotcha_result')->middleware("check_user_authentication");
