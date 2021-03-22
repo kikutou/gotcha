6c319cafa27e7f6b04653d9e05e7fcebcc142b89
chenjunyan <kitinc198744@hotmail.com>
2021-03-22 10:41:54 +0900

    表示レイアウト修正

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,22 +3,22 @@
         <div class="menu-list">
             <nav class="navbar navbar-expand-lg navbar-light">
                     <ul class="navbar-nav flex-column">
-                        <li class="nav-divider">
+                        <li class="nav-item">
                             <a class="nav-link h4
                             {{ isset($title) && $title == "ガチャ-ガチャ" ? "active" : "" }}
                             " href="{{url('gotcha')}}">ガチャ一覧</a>
                         </li>
-                        <li class="nav-divider">
+                        <li class="nav-item">
                             <a class="nav-link h4
                             {{ isset($title) && $title == "ガチャ-景品" ? "active" : "" }}
                             " href="{{url('prize')}}">景品一覧</a>
                         </li>
-                        <li class="nav-divider">
+                        <li class="nav-item">
                             <a class="nav-link h4
                             {{ isset($title) && $title == "ガチャ-画像" ? "active" : "" }}
                             " href="{{url('picture')}}">画像一覧</a>
                         </li>
-                        <li class="nav-divider">
+                        <li class="nav-item">
                             <a class="nav-link h4
                             {{ isset($title) && $title == "ログ一覧" ? "active" : "" }}
                             " href="{{url('log')}}">ログ一覧</a>
